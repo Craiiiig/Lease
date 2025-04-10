cd5db443fa001c28e89c4a6d2d447b3f52564197
Craiiiig <chasellau98@gmail.com>
2025-04-10 16:25:33 +1000

    .

--- model/src/main/java/com/atguigu/lease/model/entity/AttrKey.java
+++ model/src/main/java/com/atguigu/lease/model/entity/AttrKey.java
@@ -7,14 +7,14 @@ import lombok.Data;
 
 import java.util.List;
 
-@Schema(description = "房间基本属性表")
+@Schema(description = "Room Attribute Key Table")
 @TableName(value = "attr_key")
 @Data
 public class AttrKey extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
 
-    @Schema(description = "属性key")
+    @Schema(description = "Attribute Key Name")
     @TableField(value = "name")
     private String name;
 
--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/AttrController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/AttrController.java
@@ -1,48 +1,66 @@
 package com.atguigu.lease.web.admin.controller.apartment;
 
-
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.AttrKey;
 import com.atguigu.lease.model.entity.AttrValue;
+import com.atguigu.lease.web.admin.service.AttrKeyService;
+import com.atguigu.lease.web.admin.service.AttrValueService;
 import com.atguigu.lease.web.admin.vo.attr.AttrKeyVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
-@Tag(name = "房间属性管理")
+/**
+ * Attribute name (key) is the category of the attribute.
+ * Attribute value is the specific item under a category.
+ */
+@Tag(name = "Room Attribute Management")
 @RestController
 @RequestMapping("/admin/attr")
 public class AttrController {
 
-    @Operation(summary = "新增或更新属性名称")
+    @Autowired
+    private AttrKeyService attrKeyService;
+
+    @Autowired
+    private AttrValueService attrValueService;
+
+
+    @Operation(summary = "Add or Update Attribute Name")
     @PostMapping("key/saveOrUpdate")
     public Result saveOrUpdateAttrKey(@RequestBody AttrKey attrKey) {
-        return Result.ok();
+        boolean b = attrKeyService.saveOrUpdate(attrKey);
+        if (b) return Result.ok();
+        return Result.fail();
+
+
     }
 
-    @Operation(summary = "新增或更新属性值")
+    @Operation(summary = "Add or Update Attribute Value")
     @PostMapping("value/saveOrUpdate")
     public Result saveOrUpdateAttrValue(@RequestBody AttrValue attrValue) {
-        return Result.ok();
+        boolean b = attrValueService.saveOrUpdate(attrValue);
+        if (b) return Result.ok();
+        return Result.fail();
     }
 
-
-    @Operation(summary = "查询全部属性名称和属性值列表")
+    @Operation(summary = "Get All Attribute Names and Values")
     @GetMapping("list")
     public Result<List<AttrKeyVo>> listAttrInfo() {
+       List<AttrKeyVo> list =  attrKeyService.listAttrInfo();
         return Result.ok();
     }
 
-    @Operation(summary = "根据id删除属性名称")
+    @Operation(summary = "Delete Attribute Name by ID")
     @DeleteMapping("key/deleteById")
     public Result removeAttrKeyById(@RequestParam Long attrKeyId) {
         return Result.ok();
     }
 
-    @Operation(summary = "根据id删除属性值")
+    @Operation(summary = "Delete Attribute Value by ID")
     @DeleteMapping("value/deleteById")
     public Result removeAttrValueById(@RequestParam Long id) {
         return Result.ok();
